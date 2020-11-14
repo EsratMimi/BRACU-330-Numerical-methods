960b410201d5da4481bc3a9d41415bf9ccf77cb1
Mimi <34329276+EsratMimi@users.noreply.github.com>
2020-11-14 18:13:10 +0600

    Create central_difference.m

---
+++ Lab7/central_difference.m
@@ -0,0 +1,19 @@
+function [d]= central_difference(f,x,tol,max)
+h=1;
+D(1) = (f(x+h)-f(x))/(2*h);
+E(1) = 0;
+for n = 1:2
+    h=h/10;
+    D(n+1)=(f(x+h)-f(x))/(2*h);
+    E(n+1)=abs(D(n+1)-D(n));
+end
+
+while((E(n)>E(n+1)) && E(n)>tol && n<max)
+    h=h/10;
+    D(n+2)=(f(x+h)-f(x))/(2*h);
+    E(n+2)=abs(D(n+2)-D(n+1));
+    n=n+1;
+end
+d=D(n+1);
+%f=@(x) exp(x);
+%[d] = central_difference(f,1,.0001,12)
